0ee054f40776a094292955cc1a02f2de49c53e16
Chourouq <c_batiche@estin.dz>
2025-02-11 14:56:05 +0100

    Add S1 and S2 folders for 2cs-tp

---
+++ S2/TP SChrono/TP1/tp1sc.R
@@ -0,0 +1,22 @@
+A = matrix(0,nrow-2.ncol-5)
+
+A
+
+A[1, 2] < -3
+
+m = matrix(c(1:9),ncol=3)
+ m[2.3]
+
+m[,3]
+m[1,]
+
+ 3 + m
+1.5  * m
+m ^  2
+dim(m)
+
+ t(m)
+ det (m)
+
+ diag(m)
+
---
+++ S2/TP SChrono/TP2/tp2.R
@@ -0,0 +1,43 @@
+if(A>0){
+  print('a est un nombre positif')
+}
+A<- -5
+ if(A>0){
+  print('a est un nombre positif')
+} else {print ('A est nul ou a est un nombre negatif')
+}
+f= function(x)
+return(x^2+1)
+f(8)
+x=array(0,5)
+for(i in 1:5)
+x[i]=5-i
+x
+N=10
+q=0.8
+S=0
+for(n in 0:(N-1)){
+  S=S+q
+}
+print(S)
+getwd()
+setwd('/Users/aicha/Desktop/2CS/S2/TP SChrono')
+args(read_excel)
+read_excel('Classeur1.xlsx')
+read_excel('Classeur1.xlsx',sheet=1)
+#Exercixe7.1
+a=c('janvier','fevrier','mars','avril','mai')
+length(a)
+a[1:3]
+b<- a[c(1,4)]
+b
+a[-1]
+sort(a)
+#Exercice7.2
+vec1 <- seq(2,50,by=2)
+vec2 <- rep(0:9,each=3)
+vec3
+#Exercice7.3
+paste('chr',1,sep='')
+vec4 <- c(paste('chr',1:22,sep=''),ch)
+
